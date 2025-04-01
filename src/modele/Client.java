d9a131c6ad7ba7dcf8dae847cbfb2cce3e028fef
mathis <mathis.bretonne@edu.ece.fr>
2025-04-01 09:10:17 +0200

    ajout info paiement

--- src/modele/Client.java
+++ src/modele/Client.java
@@ -1,18 +1,41 @@
 package modele;
 
+import java.time.LocalDate;
+
 public class Client extends Utilisateur {
+    protected String prenom;
     protected String adresse;
     protected String telephone;
+    protected String numeroCarte;
+    protected LocalDate dateInscription;
 
-    public Client(int id, String nom, String email, String motDePasse, String adresse, String telephone) {
+    public Client(int id, String nom, String prenom, String email, String motDePasse,
+                  String adresse, String telephone, String numeroCarte, LocalDate dateInscription) {
         super(id, nom, email, motDePasse);
+        this.prenom = prenom;
         this.adresse = adresse;
         this.telephone = telephone;
+        this.numeroCarte = numeroCarte;
+        this.dateInscription = dateInscription;
     }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
     public String getAdresse() {
         return adresse;
     }
+
     public String getTelephone() {
         return telephone;
+    }
+
+    public String getNumeroCarte() {
+        return numeroCarte;
+    }
+
+    public LocalDate getDateInscription() {
+        return dateInscription;
     }
 }
