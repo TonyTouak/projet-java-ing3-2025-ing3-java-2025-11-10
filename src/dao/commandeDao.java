679e2f8234e0e7709bb939b66f859c544975a493
TonyTouak <tony.touak@edu.ece.fr>
2025-04-01 10:23:01 +0200

    Ajout de l'implémentation d'une commande.

--- src/dao/commandeDao.java
+++ src/dao/commandeDao.java
@@ -1,4 +1,15 @@
 package dao;
 
+import modele.Commande;
+
+import java.util.ArrayList;
+
 public interface commandeDao {
+    ArrayList<Commande> getAll();
+
+    void ajouter(Commande commande);
+
+    Commande chercher(int idCommande);
+
+    void supprimer(Commande commande);
 }
